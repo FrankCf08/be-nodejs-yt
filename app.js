0aed3c17a1c1f384bdbc74321c607b4d4967cd46
Frank Cruz <frankcruz0806@gmail.com>
2021-11-06 20:45:57 -0500

    Init NodejJs project
    * Set nodejs with nodemo.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express =  require('express');
+
+const app = express()
+
+// Server config
+app.listen(8080)
+
+// Middleware
+
+// app.use('/posts', ()=> {
+//   console.log('This is a middleware running');
+// })
+
+app.use('*', ()=> {
+  console.log('This is a middleware running');
+})
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('We are on home')
+})
+
+app.get('/posts', (req, res) => {
+  res.send('We are on Posts')
+})
