7e5b0ce96ee828d850a0179a1b80b754a1f05f5b
Frank Cruz <frankcruz0806@gmail.com>
2021-11-08 20:42:34 -0500

    Connecting to DB
    * Using "mongosse" we are able to connect to MongoDB
    * Using mlab.com, a database has been create din order to create some calls.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express =  require('express');
-
+const mongoose =  require('mongoose')
 const app = express()
 
 // Server config
 app.listen(8080)
 
-// Middleware
-app.use('/posts', ()=> {
-  console.log('This is a middleware running');
+// Connect to DB
+mongoose.connect(`${process.env.DB_CONNECTION}`, ()=> {
+  console.log('Connected to DB');
 })
 
 // Routes
