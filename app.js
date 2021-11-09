2aef41ac76fe9b8a292932cb0469f85a5f41f3d9
Frank Cruz <frankcruz0806@gmail.com>
2021-11-08 21:33:44 -0500

    Fix MongoDB connection * Require field was needed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express =  require('express');
 const mongoose =  require('mongoose')
 const app = express()
 
+require('dotenv/config')
+
 const postsRouter = require('./routes/posts') // Post Router
 const getsRouter = require('./routes/gets') // Get Router
 
@@ -9,9 +11,17 @@ const getsRouter = require('./routes/gets') // Get Router
 app.listen(8080)
 
 // Connect to DB
-mongoose.connect(`${process.env.DB_CONNECTION}`, ()=> {
-  console.log('Connected to DB');
-})
+mongoose.connect(
+  `${process.env.DB_CONNECTION}`, 
+  { useNewUrlParser: true }, 
+  (err) => {
+    if(err) {
+        console.log('Some problem with the connection ' + err);
+    } else {
+        console.log('The Mongoose connection is ready');
+    }
+  }
+)
 
 // Middleware
 app.use('/posts', postsRouter)
