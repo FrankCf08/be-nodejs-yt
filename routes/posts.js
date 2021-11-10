c488ea1821891b9f5f8bdaf2094d9c9fdcf07056
Frank Cruz <frankcruz0806@gmail.com>
2021-11-09 21:52:19 -0500

    Creating REQUESTS and sent to MONGODB in matlab
    * Posts have been creating to model our request data and response data.
    * Depending on the request data is sent or received.
    * Used: asyn for the req and res; await for the Post.find() and post.save();

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,42 @@
 const express = require('express');
+const Post = require('../models/Post');
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//   res.send('We are at POST route' )
-// })
+router.get('/', async (req, res) => {
+    try {
+      const posts = await Post.find();
+      res.json(posts)
+    } catch (error) {
+      res.json({ message: error })
+    }
+})
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   console.log(req.body);
-  res.send(req.body);
+  const post = new Post({
+    user: {
+      name: req.body.user.name,
+      lastName: req.body.user.lastName
+    },
+    title: req.body.title,
+    description: req.body.description
+  });
+  
+  //Creatin Promise
+  //1st Method
+  // post.save()
+  //     .then(data => {
+  //       res.json(data)
+  //     })
+  //     .catch(error => {
+  //       res.json({ message : error})
+  //     })
+  //2nd Method
+  try {
+    const postSave = await post.save();
+    res.json(postSave)
+  } catch (error) {
+    res.json({ message: error })
+  }
 })
 module.exports = router
